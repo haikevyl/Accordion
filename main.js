939f332849127187850196b1c4843c393d06451d
HuynhTran <huynhtran@lonewolf.net.fpt>
2019-11-26 17:51:56 +0700

    accordion demo

---
+++ main.js
@@ -0,0 +1,14 @@
+let acc = document.getElementsByClassName("accordion");
+
+for(let i = 0; i < acc.length; i++) {
+	acc[i].addEventListener('click', function(event) {
+		event.preventDefault();
+		acc[i].classList.toggle('active');
+		let panel = acc[i].nextElementSibling;
+		if(panel.style.display === 'block') {
+			panel.style.display = 'none'
+		}else {
+			panel.style.display = 'block'
+		}
+	})
+}
